1fb41abe4ed55eba3703046762cd38e9f6606491
LaiaJC <laia.jimenez.constanti@gmail.com>
2017-05-26 10:19:17 +0200

    Initial Version
    ShowCase - Initial Version

---
+++ app/src/navBar.js
@@ -0,0 +1,13 @@
+/**
+ * Created by LAJZ on 25/05/2017.
+ */
+
+/*app.module('navBarDirective', [])
+    .controller('navBarController', ['$scope', function ($scope) {
+    }])
+    .directive('navBar', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/src/templates/navBar.html'
+        };
+    });*/
